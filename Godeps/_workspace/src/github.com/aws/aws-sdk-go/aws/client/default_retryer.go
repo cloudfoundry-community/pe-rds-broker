ebb2d0d9cd2f2f517998a44bb83bc154fb5d9fde
Ferran Rodenas <frodenas@gmail.com>
2015-11-06 15:55:49 -0800

    Update Godeps

--- Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws/client/default_retryer.go
+++ Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws/client/default_retryer.go
@@ -1,11 +1,10 @@
-package service
+package client
 
 import (
 	"math"
 	"math/rand"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
 )
 
@@ -22,23 +21,18 @@ import (
 //    // This implementation always has 100 max retries
 //    func (d retryer) MaxRetries() uint { return 100 }
 type DefaultRetryer struct {
-	*Service
+	NumMaxRetries int
 }
 
 // MaxRetries returns the number of maximum returns the service will use to make
 // an individual API request.
-func (d DefaultRetryer) MaxRetries() uint {
-	if aws.IntValue(d.Service.Config.MaxRetries) < 0 {
-		return d.DefaultMaxRetries
-	}
-	return uint(aws.IntValue(d.Service.Config.MaxRetries))
+func (d DefaultRetryer) MaxRetries() int {
+	return d.NumMaxRetries
 }
-
-var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // RetryRules returns the delay duration before retrying this request again
 func (d DefaultRetryer) RetryRules(r *request.Request) time.Duration {
-	delay := int(math.Pow(2, float64(r.RetryCount))) * (seededRand.Intn(30) + 30)
+	delay := int(math.Pow(2, float64(r.RetryCount))) * (rand.Intn(30) + 30)
 	return time.Duration(delay) * time.Millisecond
 }
 
