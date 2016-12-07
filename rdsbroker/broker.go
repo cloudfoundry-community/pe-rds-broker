4bb040f3a09cfa3c0b7af81eaf355faae5207f31
joek <johannes.engelke@hybris.com>
2016-12-07 13:35:55 +0100

    Added debugging

--- rdsbroker/broker.go
+++ rdsbroker/broker.go
@@ -122,6 +122,11 @@ func (b *RDSBroker) Provision(context context.Context, instanceID string, detail
 	provisionParameters := ProvisionParameters{}
 	if b.allowUserProvisionParameters {
 		if err := json.Unmarshal(details.RawParameters, &provisionParameters); err != nil {
+			b.logger.Debug("unmarshal failed", lager.Data{
+				instanceIDLogKey:   instanceID,
+				detailsLogKey:      details,
+				asyncAllowedLogKey: asyncAllowed,
+			})
 			return provisioningResponse, err
 		}
 	}
